5aba87be61c12742b75223184db05ec4ea2bd06b
Saeed-Altout <saeedaltoutpro@gmail.com>
2026-01-15 00:06:21 +0300

    feat: Implement initial dashboard layout, navigation, and UI components.

---
+++ src/components/dashboard/app-sidebar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import * as React from "react";
+import {
+  IconDashboard,
+  IconInnerShadowTop,
+  IconFolderCheck,
+} from "@tabler/icons-react";
+
+import { NavMain } from "@/components/dashboard/nav-main";
+import { NavUser } from "@/components/dashboard/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Logo } from "@/components/shared/logo";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Overview",
+      url: "/dashboard",
+      icon: IconDashboard,
+    },
+    {
+      title: "Projects",
+      url: "/projects",
+      icon: IconFolderCheck,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <Logo />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
