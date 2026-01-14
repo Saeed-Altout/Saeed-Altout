2a34c736b468c09aa6fecef871fe42c8842f792d
Saeed-Altout <saeedaltoutpro@gmail.com>
2026-01-14 22:22:54 +0300

    refactor: simplify CvButton download logic and restore button functionality

--- src/components/shared/cv-button.tsx
+++ src/components/shared/cv-button.tsx
@@ -1,20 +1,21 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { FileIcon } from "lucide-react";
 
 export function CvButton() {
-  //   const downloadCV = () => {
-  //     const link = document.createElement("a");
-  //     link.href = "/cv.pdf";
-  //     link.download = "Saeed_Altout_CV.pdf";
-  //     link.click();
-  //   };
+  const downloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/cv.pdf";
+    link.download = "Saeed_Altout_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
-    <Button size="sm" variant="ghost" asChild>
-      <a href="/cv.pdf" download="Saeed_Altout_Cv.pdf">
-        <FileIcon />
-        Download CV
-      </a>
+    <Button size="sm" variant="ghost" onClick={downloadCV}>
+      <FileIcon />
+      Download Cv
     </Button>
   );
 }
