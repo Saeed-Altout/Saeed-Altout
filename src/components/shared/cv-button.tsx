6a0257dfb9a273b86265320cc4a543b43fa11f67
Saeed-Altout <saeedaltoutpro@gmail.com>
2026-01-14 22:16:05 +0300

    feat: update CvButton to use Link for direct CV download

--- src/components/shared/cv-button.tsx
+++ src/components/shared/cv-button.tsx
@@ -1,19 +1,21 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { FileIcon } from "lucide-react";
+import Link from "next/link";
 
 export function CvButton() {
-  const downloadCV = () => {
-    const link = document.createElement("a");
-    link.href = "/cv.pdf";
-    link.download = "Saeed_Altout_CV.pdf";
-    link.click();
-  };
+  //   const downloadCV = () => {
+  //     const link = document.createElement("a");
+  //     link.href = "/cv.pdf";
+  //     link.download = "Saeed_Altout_CV.pdf";
+  //     link.click();
+  //   };
 
   return (
-    <Button size="sm" variant="ghost" onClick={downloadCV}>
-      <FileIcon />
-      Download Cv
+    <Button size="sm" variant="ghost" asChild>
+      <Link href="/cv.pdf" download="Saeed_Altout_Cv.pdf">
+        <FileIcon />
+        Download CV
+      </Link>
     </Button>
   );
 }
